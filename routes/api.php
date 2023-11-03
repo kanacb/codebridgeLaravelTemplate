4342fb2945c7c3f1795ccef74b812793ab7df295
Kana Sabaratnam <37363346+kanacb@users.noreply.github.com>
2023-11-04 00:15:46 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// ~cb-class-paths~
+// ~cb-controller-paths~
 
 /*
 |--------------------------------------------------------------------------
