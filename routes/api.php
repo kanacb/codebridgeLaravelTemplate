dfd5706f87c87d65c0c78cd979de8d644da82d39
kanacb <kana@cloudbasha.com>
2023-11-04 00:40:24 +0800

    031123-4

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// ~cb-controller-paths~
+// ~cb-controller-paths~ 
 
 /*
 |--------------------------------------------------------------------------
