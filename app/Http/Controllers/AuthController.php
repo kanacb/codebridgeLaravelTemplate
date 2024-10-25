3b33aa8afa76121a534f3432c8e5ea7229db066b
CodeBridgeGenerator <kana@codebridge.my>
2024-10-25 19:05:28 +0800

    241024-2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,34 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
 
 class AuthController extends Controller
 {
     public function __construct()
     {
         $this->middleware('guest')->except([
-            'logout', 'user_dash'
+            'logout',
+            'user_dash'
         ]);
     }
 
 
     public function store(Request $request)
     {
-
-
         $results = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' =>Hash::make($request->password)
+            'password' => Hash::make($request->password)
         ]);
 
-        $credentials = $request->only('email', 'password');
+        $credentials = $results->only('email', 'password');
         return response()->json(["signin" => true, "message" => "User Created", $credentials]);
     }
 
@@ -42,21 +38,21 @@ class AuthController extends Controller
                 'email' => 'required|email',
                 'password' => 'required'
             ]);
-        } catch (ValidationException $validationException) {
-            return $this->validationErrorResponse($validationException,"validate user server", $status);
-        } catch (\Exception $exception) {
-            return $this->exceptionErrorResponse($exception,__CLASS__,__FUNCTION__,__LINE__);
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                $token =  $request->user()->createToken('LaravelSanctumAuth')->plainTextToken;
+                return response()->json([
+                    "user" => $user,
+                    "token" => $token
+                ]);
+            } else {
+                return response()->json(["login" => false, "message" => "Invalid User Credentials"]);
+            }
+        }  catch (\Exception $exception) {
+            return $this->exceptionErrorResponse($exception, __CLASS__, __FUNCTION__, __LINE__);
         }
 
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            $token =  $user->createToken('LaravelSanctumAuth')->plainTextToken;
-            return response()->json(["user" => $user,
-            "token" => $token]);
-        }
-        else {
-            return response()->json(["login" => false, "message" => "Invalid User Credentials"]);
-        }
+
     }
 
 
@@ -65,7 +61,9 @@ class AuthController extends Controller
     {
         Auth::logout();
         $user = Auth::user();
-        return response()->json(["user" => $user,
-        "logout" => true]);
+        return response()->json([
+            "user" => $user,
+            "logout" => true
+        ]);
     }
 }
